a26f1042a6ab25247d26eee7d87218d494ac49a6
taewank1m <taewank1m@yahoo.com>
2022-10-01 15:49:15 -0700

    first commit

---
+++ src/solution.js
@@ -0,0 +1,7 @@
+/*
+  Write a function that separates students into two buckets based on a given score. The end result for the "happy path" should be an array with two arrays inside of it.
+*/
+
+function partitionStudentsByScore(students, score) {}
+
+module.exports = partitionStudentsByScore;
