6462ab3ab5adc3d0926c96671f200e20c87b3fa9
taewank1m <taewank1m@yahoo.com>
2022-10-01 16:01:11 -0700

    Updated code

--- src/solution.js
+++ src/solution.js
@@ -2,6 +2,13 @@
   Write a function that separates students into two buckets based on a given score. The end result for the "happy path" should be an array with two arrays inside of it.
 */
 
-function partitionStudentsByScore(students, score) {}
+function partitionStudentsByScore(students, score) {
+  let result = [belowScore = [], aboveScore = []];
+  if (!students || !score) { return result };
+  const sort = students.forEach((student) => {
+    student.score <= score ? belowScore.push(student) : aboveScore.push(student);
+  });  
+  return result; 
+}
 
 module.exports = partitionStudentsByScore;
--- test/solution.test.js
+++ test/solution.test.js
@@ -1,1 +1,26 @@
-// Write your tests here!
+const { expect } = require("chai");
+const partitionStudentsByScore = require("../src/solution.js");
+
+describe("partitionStudentsByScore", () => {
+  const students = [
+    { name: "Leo Yeon-Joo", score: 8.9 },
+    { name: "Morgan Sutton", score: 7.4 },
+    { name: "Natalee Vargas", score: 9.2 },
+  ];
+  it("Should return two arrays of students", () => {
+    const expected = [
+      [{ name: "Morgan Sutton", score: 7.4 }],
+      [
+       { name: "Leo Yeon-Joo", score: 8.9 }, 
+       { name: "Natalee Vargas", score: 9.2 }
+      ]
+    ];
+    expect(partitionStudentsByScore(students, 8)).to.eql(expected);
+  });
+   it("Should return two arrays if students list or score invalid", () => {
+    expect(partitionStudentsByScore([], 0)).to.eql([[], []]);
+  });
+  it("Should return the one array full of students if below the score", () => {
+    expect(partitionStudentsByScore(students, 10)).to.eql([students, []]);
+  });
+});
